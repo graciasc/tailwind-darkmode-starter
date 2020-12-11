ea75daa8a827024a092a718ae487a76348038a42
Gracias claude <31932887+graciasc@users.noreply.github.com>
2020-12-11 10:43:53 -0700

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,24 @@
+
+// if you want to change the os default to dark
+// window.matchMedia("(prefers-color-scheme: dark)");
+
+// if you want to change the os default to light
+// window.matchMedia("(prefers-color-scheme: light)");
+
+//allows the toggle
+let toggle = true;
+
+const btn = document
+  .getElementById("btnToggle")
+  .addEventListener("click", (e) => {
+    
+    if (!toggle) {
+      toggle = true;
+      // removes dark from html tag
+      document.querySelector("html").classList.remove("dark");
+    } else {
+      toggle = false;
+      //adds dark to the html tag
+      document.querySelector("html").classList.add("dark");
+    }
+  });
